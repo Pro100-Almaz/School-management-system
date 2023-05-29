d53bf57fea124ea8e7dd054c471d81b7044cd0a6
almaz <almaz.amanzholuly@nu.edu.kz>
2023-05-30 01:50:22 +0600

    added word file creator

--- apps/document/views.py
+++ apps/document/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import DocumentForm
+from .models import Document
+from docx import Document as Doc
+from django.core.files.storage import FileSystemStorage
 
 
 def create_document(request):
@@ -20,4 +23,20 @@ def success_document(request):
         form = DocumentForm()
         return render(request, 'success.html', {'form': form})
 
+def generate_word_document(request, item_id):
+    data = Document.objects.get(id = item_id)
+
+    doc = Doc()
+
+    doc.add_paragraph("Your data goes here")
+
+    file_name = "example.docx"
+    file_path = f"media/{file_name}"
+
+    fs = FileSystemStorage()
+    with fs.open(file_path, "wb") as file:
+        doc.save(file)
+
+    # Save the document
+    return file_path
     
